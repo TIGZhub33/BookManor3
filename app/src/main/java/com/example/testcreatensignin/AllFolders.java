7afccee8b9f4fceea58d4adb1401fac529ab376c
Talia <taliamcpherson1@gmail.com>
2022-05-31 21:42:23 +0200

    fixed errors

--- app/src/main/java/com/example/testcreatensignin/AllFolders.java
+++ app/src/main/java/com/example/testcreatensignin/AllFolders.java
@@ -4,11 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-<<<<<<< HEAD:app/src/main/java/com/example/testcreatensignin/AllFolders.java
+
 public class AllFolders extends AppCompatActivity {
-=======
-public class AddFolder extends AppCompatActivity {
->>>>>>> 179171be5464290f06b6fb696628ec987be3c970:app/src/main/java/com/example/testcreatensignin/AddFolder.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
