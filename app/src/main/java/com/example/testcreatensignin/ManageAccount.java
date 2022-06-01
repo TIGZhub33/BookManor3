8e615ac0451c710945010fed0788b5b95916f84d
Roxanne-oss <roxy.smith2002@gmail.com>
2022-06-01 11:24:50 +0200

    buttons

--- app/src/main/java/com/example/testcreatensignin/ManageAccount.java
+++ app/src/main/java/com/example/testcreatensignin/ManageAccount.java
@@ -3,12 +3,28 @@ package com.example.testcreatensignin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class ManageAccount extends AppCompatActivity {
+public class ManageAccount extends AppCompatActivity
+{
+    //Declarations
+    private Button addProfilePictureButton, updateDetailsButton, deleteAccountButton;
+    private ImageView profilePictureImageView;
+    private EditText fullNameEt, emailEt, phoneEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_account);
+
+        addProfilePictureButton.findViewById(R.id.addProfilePictureButton);
+        updateDetailsButton.findViewById(R.id.updateDetailsButton);
+        deleteAccountButton.findViewById(R.id.deleteAccountButton);
+        profilePictureImageView.findViewById(R.id.profilePictureImageView);
+        fullNameEt.findViewById(R.id.fullNameEt);
+        emailEt.findViewById(R.id.emailEt);
+        addProfilePictureButton.findViewById(R.id.phoneEt);
     }
 }
--- app/src/main/java/com/example/testcreatensignin/Settings.java
+++ app/src/main/java/com/example/testcreatensignin/Settings.java
@@ -1,14 +1,70 @@
 package com.example.testcreatensignin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Settings extends AppCompatActivity {
-
+    //Declarations
+    CardView manageAccountCv, inviteFriendsCv, notificationsCv, helpAndSupportCv, appInfoCv, signOutCv;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        manageAccountCv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(Settings.this, ManageAccount.class));
+            }
+        });
+
+        inviteFriendsCv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
+
+        notificationsCv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(Settings.this, Notifications.class));
+            }
+        });
+
+        helpAndSupportCv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+
+            }
+        });
+
+        appInfoCv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
+
+        signOutCv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+
+
+            }
+        });
     }
 }
