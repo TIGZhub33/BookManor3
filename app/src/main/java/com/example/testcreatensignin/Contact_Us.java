36462eef41e5468d7abf4fef389a532729cc880d
Roxanne-oss <roxy.smith2002@gmail.com>
2022-06-22 15:50:32 +0200

    Contact us page working properly

--- app/src/main/java/com/example/testcreatensignin/Contact_Us.java
+++ app/src/main/java/com/example/testcreatensignin/Contact_Us.java
@@ -3,12 +3,17 @@ package com.example.testcreatensignin;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Contact_Us extends AppCompatActivity {
 
     private Toolbar toolbar;
-
+    private EditText edtContact;
+    private Button btnSendEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +24,33 @@ public class Contact_Us extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        edtContact = findViewById(R.id.edtContact);
+        btnSendEmail = findViewById(R.id.btnSendEmail);
+
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                sendMail();
+            }
+        });
+
+    }
+
+    private void sendMail()
+    {
+        String rec = "bmbookmanor@gmail.com";
+        String[] recipient = rec.split(" ");
+        String subject = "Customer recommendation";
+        String message = edtContact.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipient);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose an email client"));
+
     }
 }
