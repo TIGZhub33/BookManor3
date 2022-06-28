37726421b6f6c3dd2dbbf994e0de9e7bcd4754d7
Talia <taliamcpherson1@gmail.com>
2022-06-29 00:50:48 +0200

    user manual download button

--- app/src/main/java/com/example/testcreatensignin/How_Book_Manor_Works.java
+++ app/src/main/java/com/example/testcreatensignin/How_Book_Manor_Works.java
@@ -1,12 +1,29 @@
 package com.example.testcreatensignin;
 
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class How_Book_Manor_Works extends AppCompatActivity {
 
+    private Button downloadButton;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    StorageReference ref;
     private Toolbar toolbar;
 
     @Override
@@ -19,5 +36,46 @@ public class How_Book_Manor_Works extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        downloadButton = findViewById(R.id.downloadButton);
+
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                download();
+            }
+        });
+
+
+    }
+
+    public void download(){
+        storageReference=firebaseStorage.getInstance().getReference();
+        ref=storageReference.child("User Manual.pdf");
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String url=uri.toString();
+                downloadFiles(How_Book_Manor_Works.this, "User Manual", ".pdf", DIRECTORY_DOWNLOADS,url);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+    }
+
+    public void downloadFiles(Context context, String fileName, String fileExtension, String destinationDirectory, String url) {
+        DownloadManager downloadmanager = (DownloadManager) context.
+                getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request (uri);
+
+        request.setNotificationVisibility (DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir (context, destinationDirectory, fileName + fileExtension);
+        downloadmanager.enqueue(request);
     }
 }
